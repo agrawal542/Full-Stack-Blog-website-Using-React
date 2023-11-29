416d6c8868486bbbec85c165217c4493511e3010
agrawal542 <gautamagrawal542@gmail.com>
2023-11-29 12:41:47 +0530

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
+
+const store = configureStore({
+    reducer: {
+        auth : authSlice,
+        //TODO: add more slices here for posts post : Slice
+    }
+});
+
+
+export default store;
